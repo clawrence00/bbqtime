96fdf726fb66c7b5b59119d0fdc03108f7e19339
clawrence00 <>
2023-09-27 18:26:41 +0000

    added shop view and template

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ A view to show all products and include sorting and search queries """
+
+    shop = Product.objects.all()
+
+    context = {
+        'shop': shop,
+    }
+
+    return render(request, 'shop/shop.html', context)
