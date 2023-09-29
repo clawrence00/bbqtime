45d1b7b5c12981cfd51be23e5193617d9c62bc8a
clawrence00 <>
2023-09-29 19:09:57 +0000

    added checkout views and templates

--- bbq_time/settings.py
+++ bbq_time/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'shop',
     'basket',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'bbq_time.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +82,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'basket.contexts.basket_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
