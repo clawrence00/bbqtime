1842536a08ffbbd6395d3d64fc2bbbd814006450
clawrence00 <>
2023-09-29 18:27:01 +0000

    added checkout app and models

--- bbq_time/settings.py
+++ bbq_time/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'shop',
     'basket',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -73,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'basket.contexts.basket_contents',
             ],
         },
