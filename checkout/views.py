cf7d58cb82796ece45632f2991b785b239064243
clawrence00 <>
2023-09-29 19:56:06 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NvmZyLUzhYEPKBM4LlTZfl9dAGF4UppzpjaG9axJADeYyEMCAIVzwr8TJnG6LB4wFfdAfLHfNNmh3S1hdooJPK700iEiNftxe',
+        
     }
 
     return render(request, template, context)
