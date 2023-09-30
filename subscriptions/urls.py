183b4039a3f59054d1cde946e759daed9848de1e
clawrence00 <>
2023-09-30 16:23:22 +0000

    adding subscription feature

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.subscribe, name='subscribe'),
+    path('config/', views.stripe_config),
+    path('create-checkout-session/', views.create_checkout_session),
     ]
